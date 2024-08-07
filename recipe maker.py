e3018bb13d1c224eb44f4f4e611e9bd6c54f3b71
Karuru Midhun Sai <164469674+midhunsai04@users.noreply.github.com>
2024-08-07 20:45:46 +0530

    Add files via upload

---
+++ recipe maker.py
@@ -0,0 +1,7 @@
+food = input("Name of the food: ")
+plant = input("Name of the plant: ")
+methodofcooing = input("Name of the method of cooking: ")
+burnedfood = input("What word describes burned food: ")
+DIYitem = input("Name of the DIY item: ")
+print("Menu")
+print(methodofcooing,food,"with",burnedfood,plant,"on a bed of",DIYitem)
